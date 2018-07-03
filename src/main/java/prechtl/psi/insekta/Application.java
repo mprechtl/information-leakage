baefe5aaf9638fd95e751dea9c10a81d356c6225
Mike Prechtl <prechtl.mi@gmail.com>
2018-07-03 20:49:15 +0200

    Add JAX-RS Application

---
+++ src/main/java/prechtl/psi/insekta/Application.java
@@ -0,0 +1,12 @@
+package prechtl.psi.insekta;
+
+import javax.ws.rs.ApplicationPath;
+
+
+/**
+ * @author Mike Prechtl
+ */
+@ApplicationPath("/")
+public class Application extends javax.ws.rs.core.Application {
+
+}
