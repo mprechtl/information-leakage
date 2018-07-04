cedf6cdb040afe2d818994adeb28259759c32a93
Mike Prechtl <prechtl.mi@gmail.com>
2018-07-04 15:09:00 +0200

    Add Song model class

---
+++ src/main/java/prechtl/psi/insekta/model/Song.java
@@ -0,0 +1,42 @@
+
+package prechtl.psi.insekta.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ *
+ * @author Mike Prechtl
+ */
+@XmlRootElement
+public class Song {
+
+    private String title;
+    private String artist; // name of artist
+    private double duration; // in seconds
+
+    public String getTitle() {
+	return title;
+    }
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+
+    public String getArtist() {
+	return artist;
+    }
+
+    public void setArtist(String artist) {
+	this.artist = artist;
+    }
+
+    public double getDuration() {
+	return duration;
+    }
+
+    public void setDuration(double duration) {
+	this.duration = duration;
+    }
+
+}
