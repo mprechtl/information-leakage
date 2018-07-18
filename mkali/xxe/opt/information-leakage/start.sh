e22c6c5b8ba67a31132df06865dc7263967e88fd
Mike Prechtl <prechtl.mi@gmail.com>
2018-07-18 15:16:22 +0200

    Add project settings file to mkali image

--- mkali/xxe/opt/information-leakage/start.sh
+++ mkali/xxe/opt/information-leakage/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-command="java -jar /opt/information-leakage/xxe-thorntail.jar"
+command="java -jar /opt/information-leakage/xxe-thorntail.jar -s /opt/information-leakage/project-defaults.yml"
 echo "Executing: ${command}"
 echo "######################################################################"
 eval ${command}
